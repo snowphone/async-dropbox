0a6398e28d536c571820cbe76dca86524acf9e69
Junoh Moon <mjo970625@gmail.com>
2021-05-26 13:49:20 +0900

    Forked from snowphone/Sixtyfive

---
+++ src/main/kotlin/kr/sixtyfive/Response.kt
@@ -0,0 +1,18 @@
+package kr.sixtyfive
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Response(
+    val name: String,
+    val path_lower: String,
+    val path_display: String,
+    val id: String,
+    val client_modified: String,
+    val server_modified: String,
+    val rev: String,
+    val size: Long,
+    val is_downloadable: Boolean,
+    val content_hash: String,
+)
+
